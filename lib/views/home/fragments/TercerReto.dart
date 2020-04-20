e64ca4d87545813046d7b3185cd831bb24259ea2
Angel Vargas <angelvargaspool@gmail.com>
2020-04-20 01:09:35 -0500

    bluefish y tercer reto platzi

--- lib/views/home/fragments/TercerReto.dart
+++ lib/views/home/fragments/TercerReto.dart
@@ -9,7 +9,79 @@ class TercerRetoState extends State<TercerReto> {
   @override
   Widget build(BuildContext context) {
     return ListView(
-      children: <Widget>[],
+      // crossAxisAlignment: CrossAxisAlignment.start,
+      scrollDirection: Axis.vertical,
+      children: <Widget>[
+        Review("assets/img/persona1.jpg", "Miguel Lopez", "Experiencia : 1 años"),
+        Review("assets/img/persona2.png", "Antonio Perez", "Experiencia : 2 años"),
+        Review("assets/img/persona3.png", "Mariana Mendez", "Experiencia : 3 años"),
+        Review("assets/img/persona1.jpg", "Mariana Mendez", "Experiencia : 4 años"),
+        Review("assets/img/persona2.png", "Mariana Mendez", "Experiencia : 5 años"),
+        Review("assets/img/persona3.png", "Mariana Mendez", "Experiencia : 6 años"),
+        Review("assets/img/persona1.jpg", "Mariana Mendez", "Experiencia : 7 años"),
+        Review("assets/img/persona2.png", "Mariana Mendez", "Experiencia : 8 años"),
+        Review("assets/img/persona3.png", "Mariana Mendez", "Experiencia : 9 años"),
+      ],
+    );
+  }
+}
+
+
+class Review extends StatelessWidget {
+  String pathImage = "asset/img/people.jpg";
+  String name = "Varuna Yasas";
+  String details = "1 review 5 Photos";
+
+  Review(this.pathImage, this.name, this.details);
+  @override
+  Widget build(BuildContext context) {
+
+    final userInfo = Container(
+      margin: EdgeInsets.only(left: 20.0),
+      child: Text(
+        details,
+        textAlign: TextAlign.left,
+        style: TextStyle(
+            fontFamily: "Lato", fontSize: 13, color: Color(0xFFa3a5a7)),
+      ),
+    );
+
+    final username = Container(
+      margin: EdgeInsets.only(left: 20.0),
+      child: Text(
+        name,
+        textAlign: TextAlign.left,
+        style: TextStyle(fontFamily: "Lato", fontSize: 17.0),
+      ),
+    );
+
+    final userDetails = Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[username, userInfo],
+    );
+
+    final photo = Container(
+      margin: EdgeInsets.only(top: 20.0, left: 20.0),
+      width: 80.0,
+      height: 80.0,
+      decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          image:
+              DecorationImage(fit: BoxFit.cover, image: AssetImage(pathImage))),
+    );
+    
+    final actions = Container(
+      margin: EdgeInsets.only(top: 20.0, left: 20.0),
+      width: 80.0,
+      height: 80.0,
+      decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          image:
+              DecorationImage(fit: BoxFit.cover, image: AssetImage(pathImage))),
+    );
+
+    return Row(
+      children: <Widget>[photo, userDetails, actions],
     );
   }
 }
--- lib/views/home/fragments/bluefish.dart
+++ lib/views/home/fragments/bluefish.dart
@@ -1,14 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:dbcrypt/dbcrypt.dart';
+
 
 class Bluefish extends StatefulWidget {
   @override
   _BluefishState createState() => _BluefishState();
 }
 class _BluefishState extends State<Bluefish> {
+  DBCrypt dBCrypt;
+  String plainPassword;
+  String hashedPassword;
+  String resp;
+  _BluefishState(){
+    DBCrypt dBCrypt = DBCrypt();
+    plainPassword = "super";
+
+    String salt = dBCrypt.gensaltWithRounds(7);
+    // hashedPassword = dBCrypt.hashpw(plainPassword, salt);
+    // hashedPassword = "\$2x\$07\$5cDaDpgH0kIquwqDKtlUWel6kM5e6xOUzM37oLMqmnh0LQCh5R4ba";
+    // hashedPassword = "\$2y\$08\$T5lqWnzVfNJ6q6WTcJRJROeA.AcDjXHvJzKnc1v4d37figlg.zNr6";
+    hashedPassword = "\$2b\$08\$T5lqWnzVfNJ6q6WTcJRJROeA.AcDjXHvJzKnc1v4d37figlg.zNr6";
+    // hashedPwd = dBCrypt.hashpw(plainPwd, salt);
+
+    var isCorrect = new DBCrypt().checkpw(plainPassword, hashedPassword);
+    resp = isCorrect ? 'True' :'False';
+  }
+  // 
+  // static const 
+  // String 
+
+  // 
+  // 
   @override
   Widget build(BuildContext context) {
     return ListView(
-      children: <Widget>[],
+      children: <Widget>[
+        Text(hashedPassword),
+        Text(resp)
+      ],
     );
   }
 }
